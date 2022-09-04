023d30600a66f4822efd10848c8dbae327345eb4
catadecaboteau <cdecaboteau@gmail.com>
2022-09-04 16:12:05 -0300

    agregando mas cosas

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const express = require('express');
 const app = express();
 app.listen(8000, () => console.log('Levantando un servidor con Express'))
+
+app.get("/", (req, res) => {
+    res.render('home')
+})
